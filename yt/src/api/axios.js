d69be3bff8fc115bc427d7e46e8dd6dbdbcfb105
suchit1010p <suchit1564@gmail.com>
2026-01-15 17:10:48 +0530

    Connect frontend to production backend

--- yt/src/api/axios.js
+++ yt/src/api/axios.js
@@ -1,8 +1,18 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:8000/api/v1",
+//   withCredentials: true, // REQUIRED for cookies
+// });
+
+// export default api;
+
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
-  withCredentials: true, // REQUIRED for cookies
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
 
 export default api;
